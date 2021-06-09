05e285392c75ace9c14d1c97965aafd4633fa253
kumar <chowdarysmk@gmail.com>
2021-06-09 22:20:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Visiter Counter widget - PHP</title>
+    <meta name="keywords" content="counter,visit the page,Mahesh Surapaneni,Mahesh Kumar Surapaneni,web developer">
+     <link rel="shortcut icon" href="https://res.cloudinary.com/dzo3hbemf/image/upload/v1623215079/bg/mah_yskjlm.jpg" type="image/jpeg">
+    <meta name="google-site-verification" content="ZKyT8ToCwRmktXMN-ebErWjlWTl7VdsN2ftrPj3E2Go" />   
+    <meta name="description" content="Grab the count from visit list">
+    <meta charset="utf-8">
+		<meta name="generator" content="Bootply" />
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<style>body {padding-top: 70px;}</style>
+	</head>
+	<body>
+
+<div class="container-fluid">
+  
+ 
+  <div class="col-sm-9">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3>Website Hit Counter in PHP</h3>
+ <hr>
+<h4>When you refresh the page hitcount print the value with 1 Increment</h4>
+		
+Hit counts : <?php include("counter.php");?>
+      </div>
+    </div>
+    <hr>
+        
+   
+  </div>
+  <hr>
+</div>
+	</body>
+</html>
